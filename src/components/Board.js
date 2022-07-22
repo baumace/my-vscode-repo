db158fc2ce0eb26720f27642cbf540c19cfbd14e
baumace <jaybaum.03@gmail.com>
2022-07-21 22:48:29 -0400

    starting framework w/ intial css

---
+++ src/components/Board.js
@@ -0,0 +1,77 @@
+import React, { useEffect, useState } from "react";
+import { boardDefault } from '../Entry';
+import Cell from "./Cell";
+
+function Board () {
+    const [board, setBoard] = useState(boardDefault);
+    return (
+        <div class = "board">
+            <div class = "row">
+                <Cell infoIdentifier={0} attemptVal={0}/>
+                <Cell infoIdentifier={1} attemptVal={0}/>
+                <Cell infoIdentifier={2} attemptVal={0}/>
+                <Cell infoIdentifier={3} attemptVal={0}/>
+                <Cell infoIdentifier={4} attemptVal={0}/>
+                <Cell infoIdentifier={5} attemptVal={0}/>
+            </div>
+            <div class = "row">
+                <Cell infoIdentifier={0} attemptVal={1}/>
+                <Cell infoIdentifier={1} attemptVal={1}/>
+                <Cell infoIdentifier={2} attemptVal={1}/>
+                <Cell infoIdentifier={3} attemptVal={1}/>
+                <Cell infoIdentifier={4} attemptVal={1}/>
+                <Cell infoIdentifier={5} attemptVal={1}/>
+            </div>
+            <div class = "row">
+                <Cell infoIdentifier={0} attemptVal={2}/>
+                <Cell infoIdentifier={1} attemptVal={2}/>
+                <Cell infoIdentifier={2} attemptVal={2}/>
+                <Cell infoIdentifier={3} attemptVal={2}/>
+                <Cell infoIdentifier={4} attemptVal={2}/>
+                <Cell infoIdentifier={5} attemptVal={2}/>
+            </div>
+            <div class = "row">
+                <Cell infoIdentifier={0} attemptVal={3}/>
+                <Cell infoIdentifier={1} attemptVal={3}/>
+                <Cell infoIdentifier={2} attemptVal={3}/>
+                <Cell infoIdentifier={3} attemptVal={3}/>
+                <Cell infoIdentifier={4} attemptVal={3}/>
+                <Cell infoIdentifier={5} attemptVal={3}/>
+            </div>
+            <div class = "row">
+                <Cell infoIdentifier={0} attemptVal={4}/>
+                <Cell infoIdentifier={1} attemptVal={4}/>
+                <Cell infoIdentifier={2} attemptVal={4}/>
+                <Cell infoIdentifier={3} attemptVal={4}/>
+                <Cell infoIdentifier={4} attemptVal={4}/>
+                <Cell infoIdentifier={5} attemptVal={4}/>
+            </div>
+            <div class = "row">
+                <Cell infoIdentifier={0} attemptVal={5}/>
+                <Cell infoIdentifier={1} attemptVal={5}/>
+                <Cell infoIdentifier={2} attemptVal={5}/>
+                <Cell infoIdentifier={3} attemptVal={5}/>
+                <Cell infoIdentifier={4} attemptVal={5}/>
+                <Cell infoIdentifier={5} attemptVal={5}/>
+            </div>
+            <div class = "row">
+                <Cell infoIdentifier={0} attemptVal={6}/>
+                <Cell infoIdentifier={1} attemptVal={6}/>
+                <Cell infoIdentifier={2} attemptVal={6}/>
+                <Cell infoIdentifier={3} attemptVal={6}/>
+                <Cell infoIdentifier={4} attemptVal={6}/>
+                <Cell infoIdentifier={5} attemptVal={6}/>
+            </div>
+            <div class = "row">
+                <Cell infoIdentifier={0} attemptVal={7}/>
+                <Cell infoIdentifier={1} attemptVal={7}/>
+                <Cell infoIdentifier={2} attemptVal={7}/>
+                <Cell infoIdentifier={3} attemptVal={7}/>
+                <Cell infoIdentifier={4} attemptVal={7}/>
+                <Cell infoIdentifier={5} attemptVal={7}/>
+            </div>
+        </div>
+    );
+}
+
+export default Board
---
+++ src/components/Cell.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Cell({infoIdentifier, attemptVal}){
+    return (
+        <div>Cell</div>
+    );
+}
+
+export default Cell;
