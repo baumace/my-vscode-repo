388cf8382677dffad2e85013fadb237c3255a383
baumace <jaybaum.03@gmail.com>
2022-08-08 15:22:36 -0400

    basic game over screen messages

--- src/components/GameOver.js
+++ src/components/GameOver.js
@@ -2,9 +2,34 @@ import React, { useContext } from "react";
 import { AppContext } from "../App";
 
 function GameOver() {
-  const { gameOver, setGameOver } = useContext(AppContext);
+  const { gameOver, currAttempt, correctPlayer } = useContext(AppContext);
 
-  return <div className="gameOver">Game Over</div>;
+  const winScreen = () => {
+    return (
+      <div>
+        <p>Draft grade: A+</p>
+        <p>
+          You guessed the correct player in {currAttempt.attempt - 1}{" "}
+          selections!
+        </p>
+      </div>
+    );
+  };
+
+  const loseScreen = () => {
+    return (
+      <div>
+        <p>Draft bust! You were not able to select the correct player.</p>
+        <p>The correct player is {correctPlayer.player}</p>
+      </div>
+    );
+  };
+
+  return (
+    <div className="gameOver">
+      {gameOver.guessedPlayer ? winScreen() : loseScreen()}
+    </div>
+  );
 }
 
 export default GameOver;
