5c0c253951baf990ff351bd3a1c9b92b029a636c
baumace <jaybaum.03@gmail.com>
2022-08-12 17:39:08 -0400

    added rough dropdown menu to filter years

--- src/components/Settings.js
+++ src/components/Settings.js
@@ -5,6 +5,15 @@ import "./Settings.css";
 
 function Settings() {
   const { popupActive, setPopupActive, selectPlayer } = useContext(AppContext);
+  const [dropdownActive, setDropdownActive] = useState({ active: false });
+
+  const handleDropdownClick = () => {
+    if (dropdownActive.active) {
+      setDropdownActive({ active: false });
+    } else {
+      setDropdownActive({ active: true });
+    }
+  };
 
   return (
     <div className="settings" id={popupActive.settings ? "show" : "hide"}>
@@ -23,6 +32,36 @@ function Settings() {
       >
         New Player
       </button>
+      <div className="eraDropdown">
+        <button
+          className="eraDropdownButton"
+          onClick={() => {
+            handleDropdownClick();
+          }}
+        >
+          Era
+        </button>
+        <div
+          className="eraDropdownContent"
+          id={dropdownActive.active ? "show" : "hide"}
+        >
+          <div className="eraDropdownItem">
+            <p>2010-Pres.</p>
+          </div>
+          <div className="eraDropdownItem">
+            <p>2000-2009</p>
+          </div>
+          <div className="eraDropdownItem">
+            <p>1990-1999</p>
+          </div>
+          <div className="eraDropdownItem">
+            <p>1980-1989</p>
+          </div>
+          <div className="eraDropdownItem">
+            <p>1968-1979</p>
+          </div>
+        </div>
+      </div>
       <div className="settingsText"></div>
     </div>
   );
