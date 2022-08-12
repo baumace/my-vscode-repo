13c3b2f8757e592fe66e202634fee77b4995006a
baumace <jaybaum.03@gmail.com>
2022-08-12 18:47:24 -0400

    dropdown click selection func

--- src/components/Help.js
+++ src/components/Help.js
@@ -5,16 +5,18 @@ function Help() {
   const { popupActive, setPopupActive } = useContext(AppContext);
 
   return (
-    <div className="help" id={popupActive.help ? "show" : "hide"}>
-      <button
-        className="exitButton"
-        onClick={() => {
-          setPopupActive({ help: false });
-        }}
-      >
-        X
-      </button>
-      <div className="helpText"></div>
+    <div className="popup" id={popupActive.settings ? "show" : "hide"}>
+      <div className="help" id={popupActive.help ? "show" : "hide"}>
+        <button
+          className="exitButton"
+          onClick={() => {
+            setPopupActive({ help: false });
+          }}
+        >
+          X
+        </button>
+        <div className="helpText"></div>
+      </div>
     </div>
   );
 }
--- src/components/Settings.js
+++ src/components/Settings.js
@@ -6,6 +6,22 @@ import "./Settings.css";
 function Settings() {
   const { popupActive, setPopupActive, selectPlayer } = useContext(AppContext);
   const [dropdownActive, setDropdownActive] = useState({ active: false });
+  const [selectedEra, setSelectedEra] = useState({ era: 0 });
+  const eraYears = [
+    "All Years",
+    "2010-Pres.",
+    "2000-2009",
+    "1990-1999",
+    "1980-1989",
+    "1968-1979",
+  ];
+
+  console.log(selectedEra.era);
+
+  const handleExitClick = () => {
+    setDropdownActive({ active: false });
+    setPopupActive({ settings: false });
+  };
 
   const handleDropdownClick = () => {
     if (dropdownActive.active) {
@@ -15,12 +31,37 @@ function Settings() {
     }
   };
 
+  const handleDropdownItemClick = (id) => {
+    if (id != selectedEra.era) {
+      switch (id) {
+        case 0:
+          setSelectedEra({ era: 0 });
+          break;
+        case 1:
+          setSelectedEra({ era: 1 });
+          break;
+        case 2:
+          setSelectedEra({ era: 2 });
+          break;
+        case 3:
+          setSelectedEra({ era: 3 });
+          break;
+        case 4:
+          setSelectedEra({ era: 4 });
+          break;
+        case 5:
+          setSelectedEra({ era: 5 });
+          break;
+      }
+    }
+  };
+
   return (
     <div className="settings" id={popupActive.settings ? "show" : "hide"}>
       <button
         className="exitButton"
         onClick={() => {
-          setPopupActive({ settings: false });
+          handleExitClick();
         }}
       >
         X
@@ -33,32 +74,66 @@ function Settings() {
         New Player
       </button>
       <div className="eraDropdown">
-        <button
+        <div
           className="eraDropdownButton"
           onClick={() => {
             handleDropdownClick();
           }}
         >
-          Era
-        </button>
+          <p>Selected Era: {eraYears[selectedEra.era]}</p>
+        </div>
+
         <div
           className="eraDropdownContent"
           id={dropdownActive.active ? "show" : "hide"}
         >
-          <div className="eraDropdownItem">
-            <p>2010-Pres.</p>
+          <div
+            className="eraDropdownItem"
+            onClick={() => {
+              handleDropdownItemClick(0);
+            }}
+          >
+            <p>{eraYears[0]}</p>
           </div>
-          <div className="eraDropdownItem">
-            <p>2000-2009</p>
+          <div
+            className="eraDropdownItem"
+            onClick={() => {
+              handleDropdownItemClick(1);
+            }}
+          >
+            <p>{eraYears[1]}</p>
           </div>
-          <div className="eraDropdownItem">
-            <p>1990-1999</p>
+          <div
+            className="eraDropdownItem"
+            onClick={() => {
+              handleDropdownItemClick(2);
+            }}
+          >
+            <p>{eraYears[2]}</p>
           </div>
-          <div className="eraDropdownItem">
-            <p>1980-1989</p>
+          <div
+            className="eraDropdownItem"
+            onClick={() => {
+              handleDropdownItemClick(3);
+            }}
+          >
+            <p>{eraYears[3]}</p>
           </div>
-          <div className="eraDropdownItem">
-            <p>1968-1979</p>
+          <div
+            className="eraDropdownItem"
+            onClick={() => {
+              handleDropdownItemClick(4);
+            }}
+          >
+            <p>{eraYears[4]}</p>
+          </div>
+          <div
+            className="eraDropdownItem"
+            onClick={() => {
+              handleDropdownItemClick(5);
+            }}
+          >
+            <p>{eraYears[5]}</p>
           </div>
         </div>
       </div>
