1a3e3aec928b787975c7a26204a8697443cd883a
Ace Baum <jaybaum.03@gmail.com>
2022-09-21 22:59:53 -0400

    commented board

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,20 +1,29 @@
 import React from "react";
 import Cell from "./Cell";
 
+/*
+ * Creates the 8 row by 6 cell board that will display the information of the game.
+ */
 function Board() {
-  const row = (attempt) => {
+  /*
+   * Returns a complete row of 6 cells to contain information.
+   */
+  function row(attemptNum) {
     return (
       <div className="row">
-        <Cell infoIdentifier={0} attemptVal={attempt} />
-        <Cell infoIdentifier={1} attemptVal={attempt} />
-        <Cell infoIdentifier={2} attemptVal={attempt} />
-        <Cell infoIdentifier={3} attemptVal={attempt} />
-        <Cell infoIdentifier={4} attemptVal={attempt} />
-        <Cell infoIdentifier={5} attemptVal={attempt} />
+        <Cell infoIdentifier={0} attemptVal={attemptNum} />
+        <Cell infoIdentifier={1} attemptVal={attemptNum} />
+        <Cell infoIdentifier={2} attemptVal={attemptNum} />
+        <Cell infoIdentifier={3} attemptVal={attemptNum} />
+        <Cell infoIdentifier={4} attemptVal={attemptNum} />
+        <Cell infoIdentifier={5} attemptVal={attemptNum} />
       </div>
     );
-  };
+  }
 
+  /*
+ ``* Return the board with 8 rows.
+   */
   return (
     <div className="board">
       {row(0)}
